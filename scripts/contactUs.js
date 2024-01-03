cc04b55525ccba605f3a827fbc358a592017e5d4
apoadel <135399429+apoadel@users.noreply.github.com>
2024-01-03 00:56:05 +0000

    ..

--- scripts/contactUs.js
+++ scripts/contactUs.js
@@ -1,9 +1,24 @@
 const form = document.querySelector(".form");
-const inputs = document.querySelectorAll("inputs");
+const inputs = document.querySelectorAll("input");
+const errMsg = document.querySelector(".err-msg");
+const successMsg = document.querySelector(".success-msg");
+console.log(inputs);
 
 console.log(form);
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    inputs.forEach(n => {
+        if (!n.value){
+            errMsg.classList.remove("d-none");
+        }
+        if(n.value){
+            successMsg.classList.remove("d-none");
+            if(!errMsg.classList.contains("d-none")){
+                errMsg.classList.add("d-none");
+            }
+        }
+
+    })
 })
 
